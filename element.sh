e0af6c0e3343205e9881527aca4b8ebd253347b8
Chen-Yuan-Lai <a186235@gmail.com>
2023-02-17 02:40:38 +0000

    fix: modify query result

--- element.sh
+++ element.sh
@@ -10,14 +10,14 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ $1 =~ ^[0-9]+$ ]]
 then
   # search information through atomic_number
-  RESULT=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING(atomic_number) WHERE atomic_number = $1")
+  RESULT=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE atomic_number = $1")
 elif [[ $1 =~ ^[A-Z]$|^[A-Z][a-z]$ ]]
 then
   # search information through symbol
-  RESULT=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING(atomic_number) WHERE symbol = '$1'")
+  RESULT=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE symbol = '$1'")
 else
   # search information through name
-  RESULT=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING(atomic_number) WHERE name = '$1'")
+  RESULT=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE name = '$1'")
 fi
 
 #if the element not exit 
@@ -27,9 +27,8 @@ then
 else
   # Set | as the delimiter
   IFS='|'
-  echo "$RESULT" | while read ATOMIC_NUMBER SYMBOL NAME TYPE ATOMIC_mass MELTING_POINT BOILING_POINT TYPE_ID
+  echo "$RESULT" | while read TYPE_ID ATOMIC_NUMBER SYMBOL NAME ATOMIC_mass MELTING_POINT BOILING_POINT TYPE
   do
     echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_mass amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
   done
-  # echo "$RESULT" 
 fi
